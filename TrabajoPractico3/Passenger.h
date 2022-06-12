6f67109fffc1456ea13248513a8162491717ad69
Lucas Torres <liqui0004@gmail.com>
2022-06-11 21:35:50 -0300

    agregando comentarios a passenger.h

--- TrabajoPractico3/Passenger.h
+++ TrabajoPractico3/Passenger.h
@@ -20,10 +20,35 @@ typedef struct
 
 }Passenger;
 
+/** \brief Passenger_new
+ * crea un nuevo pasajero.
+ * return Passenger*
+ */
+
 Passenger* Passenger_new();
+/** \brief Passenger_newparametros
+ * esta funcion crea un nuevo pasajero con todos los datos.
+ * \param char* idStr
+ * \param char* nombreStr
+ * \param char* apellidoStr
+ * \param char* FloatStr
+ * \param char* codigoVueloStr
+ * \param char* TipoPasjeroStr
+ * \param char* *statusflightStr
+ * \return Passenger* this
+ *
+ */
 Passenger* Passenger_newParametros(char* idStr,char* nombreStr,char* apellidoStr ,char* floatStr ,char* codigoVueloStr,char* tipoPasajeroStr,char*statusflightStr);
+
+/** \brief Passenger_delete
+ * esta funcion elimina un pasajero.
+ * \param Passenger* this
+ * \return void
+ *
+ */
 void Passenger_delete(Passenger* this);
 
+//getters y setters.
 int Passenger_setId(Passenger* this,int id);
 int Passenger_getId(Passenger* this,int* id);
 
@@ -46,9 +71,40 @@ int Passenger_setstatusflight(Passenger* this,char statusflight);
 int Passenger_getstatusflight(Passenger* this,char* statusflight);
 
 //funciones propias.
+
+/** \brief Passenger_print
+ * esta funcion imprime todo los datos de un pasajero.
+ * \param Passenger* this
+ * \return void
+ *
+ */
 void Passenger_print(Passenger* this);
+
+/** \brief ColocarId
+ * verifica cuantas id da y le da una id al pasajero.
+ * \param LinkedList* pArrayListPassenger
+ * \param Passenger* this
+ * \return void
+ *
+ */
+
 void colocarId(LinkedList* pArrayListPassenger , Passenger* this);
+
+/** \brief Passenger_edit
+ * Esta funcion edita un pasajero.
+ * \param Passenger* this
+ * \return void
+ *
+ */
 void Passenger_edit(Passenger* this);
+
+/** \brief Passenger_sortApellido
+ * esta funcion toma 2 pasajeros para comprar sus apellidos y devolver si es mayor menor igual.
+ * \param void* passenger1
+ * \param void* passenger1
+ * \return int
+ *
+ */
 int Passenger_SortApellido(void* passenger1 , void* passenger2);
 
 #endif /* PASSENGER_H_ */
